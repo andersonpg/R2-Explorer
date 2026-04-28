5408b16efed05e24453f8d8ca3275187a7740291
andersonpg <34556695+andersonpg@users.noreply.github.com>
2026-04-28 09:17:50 -0700

    Add users

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,10 +1,14 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
-export default R2Explorer({
-	readonly: true,
-	// basicAuth: {
-	// 	username: "username",
-	// 	password: "password",
-	// },
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'patrick',
+    password: 'Route66Recharged1$'
+  },{
+    username: 'ea',
+    password: 'Route66Recharged'
+  },{
+    username: 'liv',
+    password: 'Route66Recharged1$'
+  }]
 });
